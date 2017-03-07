a16ca1c3ed1145f317a7164b3d76f98113aed67a
Tom <t.langwaldt@gmail.com>
2017-03-07 22:37:24 +0100

    Tmp stashing

--- Client/utils.js
+++ Client/utils.js
@@ -99,38 +99,39 @@ checkForVarsInDOM();
 // Expects an object with all its settings, mainly el and data
 function NotVue(params) {
     // TODO sanity check if el is present in params object
+
     this.el = params.el; 
 
-		// during setting up getter/setter empty
-		this.data = {};
+    // during setting up getter/setter empty
+    this.data = {};
 
-		// register callbacks for every element in data
+    // register callbacks for every element in data
     for (dataElement in params.data) {
-				// Enable one way binding for all data
         // Which {{variable}} is to be replaced
         var variable = dataElement;
 
         // What to replace the variable with
         var content = params.data[dataElement];
 
-				// When data changes, update the DOM
-				Object.defineProperty(this.data, dataElement, {
-						get : function(){
-										return this._name;
-									},
+        // When data changes, update the DOM
+        Object.defineProperty(this.data, dataElement, {
+            get :   function(){
+                        return this._name;
+                    },
 
-						set: 	function(val){  
-										// TODO check if content is actually a string
-										replaceVarsInDOM(variable, content);
+            set: 	function(val){  
+                        // TODO check if content is actually a string
 
-										console.log("will change to: " + val);
-										this._name = val;
-										
-									}
-				});
+                        replaceVarsInDOM(variable, content);
+
+                        console.log("will change to: " + val);
+                        this._name = val;
+                            
+                    }
+        });
     }
 
-		// Actually copy data, once getter/setter are set up
+    // Only copy data, once getter/setter are set up
     this.data = params.data;
 }
 
@@ -146,3 +147,18 @@ var notVue = new NotVue({
 
 notVue.data.message = 'changged';
 notVue.data.test = 'changged';
+
+function scanForInputTags () {
+    // TODO only start searching from the vue el element
+    
+    // Get all elements with a nv-model attribute
+    var allElements = document.querySelectorAll("input[nv-model]") 
+
+    for (var i = 0; i < allElements.length; i++)
+    {
+        var test = allElements[i].getAttribute("nv-model");
+        console.log(test);
+    }
+}
+
+scanForInputTags();
