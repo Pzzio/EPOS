49762e93707085fe71666d9f56b1ec057f9c4bb8
Tom Langwaldt <t.langwaldt@gmail.com>
2017-03-08 11:11:55 +0100

    Implement nv-model feature

--- Client/utils.js
+++ Client/utils.js
@@ -4,97 +4,6 @@ var elementsWithVars = [];
 var elementCnt = 0;
 var lockInput = false;
 
-class VarContainer{
-    constructor(element, name){
-        this.element = element;
-        this.name = name;
-    }
-}
-
-/* Function to dynamically create HTML Lists.
- * The first argument is the list to be printed
- * The second is the action to be performed on every element before printing
- */
-function buildList(input, action){
-    for(element in input){
-        document.write('<li>' + action(element) + '</li>');
-    }
-}
-
-function doGet(){
-
-}
-
-function doPost(){
-
-}
-
-function escapeRegExp(str) {
-    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-}
-
-function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-}
-
-function replaceVarsInDOM(classToFocus){
-    if(bodyContent == undefined){
-        bodyContent = document.body.innerHTML;
-    }
-    var all = bodyContent;
-
-    for (property in $scope) {
-        if ($scope.hasOwnProperty(property)) {
-            all = replaceAll(all, "{{"+property+"}}", $scope[property]);
-        }
-    }
-    document.body.innerHTML = all;
-    if(classToFocus !== undefined && document.getElementsByClassName(classToFocus)[0] !== undefined){
-        document.getElementsByClassName(classToFocus)[0].onfocus = function(){this.value = this.value;};
-        document.getElementsByClassName(classToFocus)[0].focus();
-    }
-}
-
-function checkForVarsInDOM(){
-    var allTags = document.body.getElementsByTagName('*');
-    for(var i = 0; i < allTags.length; i++){
-        if(new RegExp('{{[a-z|A-Z]*}}', 'g').test(''+allTags[i].value)){
-            allTags[i].className += elementCnt;
-            elementCnt++;
-            var varName = (''+allTags[i].value).match(/{{[a-z|A-Z]*}}/)[0].replace('{{', '').replace('}}', '');
-            elementsWithVars.push(new VarContainer(allTags[i], varName));
-        }
-    }
-
-    document.onkeydown = function(e){
-        if(e.keyCode === 17){
-            lockInput = true;
-        }
-    }
-    document.onkeyup = function(e){
-        if(!lockInput){
-            var target = (e.target) ? e.target : e.srcElement;
-
-            for(var i = 0; i < elementsWithVars.length; i++){
-                if(elementsWithVars[i].element.className === target.className){
-                    $scope[elementsWithVars[i].name] = target.value;
-                }
-            }
-            replaceVarsInDOM(target.className);
-        }
-        else{
-            if(e.keyCode === 17){
-                lockInput = false;
-            }
-        }
-    };
-
-    replaceVarsInDOM();
-}
-
-checkForVarsInDOM();
-
-
 // NotVue's constructor
 // Expects an object with all its settings, mainly el and data
 function NotVue(params) {
@@ -150,15 +59,24 @@ notVue.data.test = 'changged';
 
 function scanForInputTags () {
     // TODO only start searching from the vue el element
-    
+
     // Get all elements with a nv-model attribute
     var allElements = document.querySelectorAll("input[nv-model]") 
 
     for (var i = 0; i < allElements.length; i++)
     {
-        var test = allElements[i].getAttribute("nv-model");
-        console.log(test);
+				var inputElement = allElements[i];
+
+				inputElement.oninput = function(inputEvent){ 
+						var inputElement = inputEvent.currentTarget;
+						var varToBind = inputElement.getAttribute("nv-model");
+
+						// TODO get the "closest" vue instance, meaning the one
+						// with the closest el value in the DOM
+						notVue.data[varToBind] = inputElement.value; 
+				};	
     }
 }
 
+// Assign oninput listeners
 scanForInputTags();
