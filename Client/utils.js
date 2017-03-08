8c59057ff904cfcf8c6ffeda026c72b237f0e2c7
goellnerphillip <goellner.phill-tit15@it.dhbw-ravensburg.de>
2017-03-08 17:04:31 +0100

    Commit changes for checkout

--- Client/utils.js
+++ Client/utils.js
@@ -4,8 +4,8 @@ var elementsWithVars = [];
 var elementCnt = 0;
 var lockInput = false;
 
-class VarContainer{
-    constructor(element, name){
+class VarContainer {
+    constructor(element, name) {
         this.element = element;
         this.name = name;
     }
@@ -15,13 +15,13 @@ class VarContainer{
  * The first argument is the list to be printed
  * The second is the action to be performed on every element before printing
  */
-function buildList(input, action){
-    for(element in input){
+function buildList(input, action) {
+    for (element in input) {
         document.write('<li>' + action(element) + '</li>');
     }
 }
 
-function setNewUrl(url, title='default'){
+function setNewUrl(url, title = 'default') {
     window.history.pushState({urlPath: url}, title, url);
 }
 
@@ -35,9 +35,9 @@ function goToCheckout() {
     setNewUrl('/checkout', 'checkout');
 }
 
-function doGet(url){
+function doGet(url) {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             changeTo(url);
             buildFromJson(JSON.parse(xhttp.responseText));
@@ -47,7 +47,7 @@ function doGet(url){
     xhttp.send();
 }
 
-function doPost(){
+function doPost() {
 
 }
 
@@ -58,53 +58,55 @@ function replaceAll(str, find, replace) {
     return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
-function replaceVarsInDOM(classToFocus){
-    if(bodyContent == undefined){
+function replaceVarsInDOM(classToFocus) {
+    if (bodyContent == undefined) {
         bodyContent = document.body.innerHTML;
     }
     var all = bodyContent;
 
     for (property in $scope) {
         if ($scope.hasOwnProperty(property)) {
-            all = replaceAll(all, "{{"+property+"}}", $scope[property]);
+            all = replaceAll(all, "{{" + property + "}}", $scope[property]);
         }
     }
     document.body.innerHTML = all;
-    if(classToFocus !== undefined && document.getElementsByClassName(classToFocus)[0] !== undefined){
-        document.getElementsByClassName(classToFocus)[0].onfocus = function(){this.value = this.value;};
+    if (classToFocus !== undefined && document.getElementsByClassName(classToFocus)[0] !== undefined) {
+        document.getElementsByClassName(classToFocus)[0].onfocus = function () {
+            this.value = this.value;
+        };
         document.getElementsByClassName(classToFocus)[0].focus();
     }
 }
 
-function checkForVarsInDOM(){
+function checkForVarsInDOM() {
     var allTags = document.body.getElementsByTagName('*');
-    for(var i = 0; i < allTags.length; i++){
-        if(new RegExp('{{[a-z|A-Z]*}}', 'g').test(''+allTags[i].value)){
+    for (var i = 0; i < allTags.length; i++) {
+        if (new RegExp('{{[a-z|A-Z]*}}', 'g').test('' + allTags[i].value)) {
             allTags[i].className += elementCnt;
             elementCnt++;
-            var varName = (''+allTags[i].value).match(/{{[a-z|A-Z]*}}/)[0].replace('{{', '').replace('}}', '');
+            var varName = ('' + allTags[i].value).match(/{{[a-z|A-Z]*}}/)[0].replace('{{', '').replace('}}', '');
             elementsWithVars.push(new VarContainer(allTags[i], varName));
         }
     }
 
-    document.onkeydown = function(e){
-        if(e.keyCode === 17){
+    document.onkeydown = function (e) {
+        if (e.keyCode === 17) {
             lockInput = true;
         }
     }
-    document.onkeyup = function(e){
-        if(!lockInput){
+    document.onkeyup = function (e) {
+        if (!lockInput) {
             var target = (e.target) ? e.target : e.srcElement;
 
-            for(var i = 0; i < elementsWithVars.length; i++){
-                if(elementsWithVars[i].element.className === target.className){
+            for (var i = 0; i < elementsWithVars.length; i++) {
+                if (elementsWithVars[i].element.className === target.className) {
                     $scope[elementsWithVars[i].name] = target.value;
                 }
             }
             replaceVarsInDOM(target.className);
         }
-        else{
-            if(e.keyCode === 17){
+        else {
+            if (e.keyCode === 17) {
                 lockInput = false;
             }
         }
