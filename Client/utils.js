2baf6ea31ad386caa524521613428bb23776df8b
Tom <t.langwaldt@gmail.com>
2017-03-09 19:25:40 +0100

    Replace vars in dom on input change

--- Client/utils.js
+++ Client/utils.js
@@ -1,4 +1,5 @@
-var dynamicElements;        // holds all elements which contain {{variable}} innerHTML
+var dynamicElements = [];        // holds all elements which contain {{variable}} innerHTML
+var internalData = {};
 
 function escapeRegExp(str) {
     return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
@@ -9,67 +10,88 @@ function replaceAll(str, find, replace) {
 }
 
 // Replace {{variableName}} with content
-function replaceVarsInDOM(variableName, content)
+function replaceVarsInDOM()
 {
+    // iterate over dynamicElements list
+    for (var i=0; i < dynamicElements.length; i++) {
+        var element = dynamicElements[i].element;
+        var initialInnerHTML = dynamicElements[i].initialContent;
+        var variableName = dynamicElements[i].variableName;
+
+        // TODO dynamically get vue object reference
+        var variableContent = notVue.data[variableName];  
+
+        if (initialInnerHTML.includes(variableName)){
+
+            var changedInner = replaceAll(initialInnerHTML, "{{"+variableName+"}}", variableContent);
+            element.innerHTML = changedInner;	
+        }
+    }
+}
+
+function registerAllDynamicElements() {
+
 		var allElements = document.getElementsByTagName("*");	
 
 		for (var i = 0; i<allElements.length; i++) {
-				var test = allElements[i];
+
+				var element = allElements[i];
 
 				// make sure only text is in innerHTML
-				if (test.children.length == 0) {
+				if (element.children.length == 0) {
+						if (element.innerHTML.length > 0) {
+                                if (element.innerHTML.match(/.*\{.*\}.*/)) {
+                                        var dynamicElement = {}; 
 
-						if (test.innerHTML.length > 0) {
-								var changedInner = replaceAll(test.innerHTML, "{{"+variableName+"}}", content);
+                                        dynamicElement.element = element;
+                                        dynamicElement.initialContent = element.innerHTML;
 
-								test.innerHTML = changedInner;	
-						}
-				}
-		}
+                                        var matches = element.innerHTML.match(/\{\{(.*?)\}\}/);
+
+                                        if (matches) {
+                                            var str = dynamicElement.variableName;
+                                            str = matches[0];
+                                            
+                                            // TODO oh god, please regex this
+                                            str = str.replace('{','');
+                                            str = str.replace('{','');
+                                            str = str.replace('}','');
+                                            str = str.replace('}','');
+
+                                            dynamicElement.variableName = str;
+                                        }  
+
+                                        dynamicElements.push(dynamicElement); 
+                                }
+                        }
+                }
+        }
 }
+
 
 // NotVue's constructor
 // Expects an object with all its settings, mainly el and data
 function NotVue(params) {
+
+    // save all element references with dynamic content in them
+    registerAllDynamicElements();
+
     // TODO sanity check if el is present in params object
 
     this.el = params.el; 
 
     // during setting up getter/setter empty
-    this.data = {};
+    this.internalData = params.data;
 
-    // register callbacks for every element in data
-    for (dataElement in params.data) {
-        // Which {{variable}} is to be replaced
-        var variable = dataElement;
-
-        // What to replace the variable with
-        var content = params.data[dataElement];
-
-        // When data changes, update the DOM
-        Object.defineProperty(this.data, dataElement, {
-            get :   function(){
-                        return this._name;
-                    },
-
-            set: 	function(val){  
-                        console.log("will change to: " + val);
-
-                        var splitStr = val.split('#'); 
-
-                        var varToReplace = splitStr[0];
-                        var content = splitStr[1]; 
-
-                        // TODO check if content is actually a string
-                        replaceVarsInDOM(varToReplace, content);
-
-                        this._name = val;
-                    }
-        });
-    }
-
-    // Only copy data, once getter/setter are set up
-    //this.data = params.data;
+    Object.defineProperty(this, 'data', { 
+        get: function() { 
+            this.updateDOM = true;
+            return this.internalData; 
+        },
+        set: function(newVal) { 
+            this.internalData = newVal; 
+        } 
+    });
 }
 
 // Create a test NotVue object
@@ -81,6 +103,8 @@ var notVue = new NotVue({
         tom:        'toms content'
     }
 });
+
+notVue.data.test = "changed test";
 
 function scanForInputTags () {
     // TODO only start searching from the vue el element
@@ -98,7 +122,9 @@ function scanForInputTags () {
 
 						// TODO get the "closest" vue instance, meaning the one
 						// with the closest el value in the DOM
-						notVue.data[varToBind] = varToBind + "#" + inputElement.value; 
+						notVue.data[varToBind] = inputElement.value; 
+
+                        replaceVarsInDOM();
 				};	
     }
 }
