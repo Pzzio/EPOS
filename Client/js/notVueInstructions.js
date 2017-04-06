2ce3a34c8a0b1b5219db6128abef2a54661f843f
Tom <t.langwaldt@gmail.com>
2017-04-06 20:52:36 +0200

    Commented notVue.js

--- Client/js/notVueInstructions.js
+++ Client/js/notVueInstructions.js
@@ -1,14 +1,14 @@
-// Creates a notVue instance and contains all dynamic data
-
+// notVue instance which holds data which is to be dynamically replaced in
+// the DOM (e.g. {{message}} will be replaced by the actual value)
 const notVue = new NotVue({
     el: '#el',
     data: {
         message: 'Herzlich Willkommen bei Rosettis Pizza',
         template_total_cart_price: 0
     }
-});;;;;;;;;;;;;;;;;;;;;;;;;;;;
+});
 
-
+// Handles when an order is to be placed
 function onOrderBtn() {
     // sanity check
     if (notVue.data.nachName &&
@@ -22,11 +22,10 @@ function onOrderBtn() {
 
         console.log("TODO send all to server");
     }
-
 }
 
 
-// Warenkorb
+// Opening and closing of the shopping cart
 let roundButton = document.querySelector("#shopping-cart-btn");
 roundButton.addEventListener("click", showMenu, false);
 
@@ -34,12 +33,14 @@ let flyoutMenu = document.querySelector("#flyoutMenu");
 let transparentMenu = document.querySelector("#transparentMenu");
 transparentMenu.addEventListener("click", hideMenu, false);
 
+// show shopping cart
 function showMenu() {
     flyoutMenu.classList.add("show");
 
     document.body.style.overflow = "hidden";
 }
 
+// hide shopping cart
 function hideMenu(e) {
     flyoutMenu.classList.remove("show");
     e.stopPropagation();
